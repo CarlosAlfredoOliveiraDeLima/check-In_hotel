f2cc61fda9a8c73215f6708ffda07327fb1d231b
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2021-01-05 00:09:30 -0300

    Transformando funções de consulta e inserção de clientes em funções independentes

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 import psycopg2
-
-
-def register_client():
-    cpf = input('Digite o CPF: ')
-    first_name = input('Digite o nome: ')
-    name = input('Digite o sobrenome: ')
-    sql = "insert into client_basic_data (cpf, client_first_name, client_name) values (%s, %s, %s)"
-    action_db.execute(sql, (cpf, first_name, name))
-    connection_db.commit()
-
-
-def consult_client():
-    cpf = input("Digite o CPF: ")
-    # cpf = input('Digite o CPF: ')
-    # action_db.execute("select client_first_name, client_name from client_basic_data where cpf = %s", cpf)
-    # datas = action_db.fetchall()
-    # print(f'Nome do cliente: {datas[0]} {datas[1]}')
-    sql = "select * from client_basic_data where cpf = %s"
-    action_db.execute(sql, (cpf,))
-    datas = action_db.fetchall()
-    for r in datas:
-        print(f'Nome do cliente: {r[2]} {r[3]} CPF: {r[1]}')
+import consult_customer
+import register_customer
 
 
 login = input("Digite o usuário: ")
@@ -36,14 +16,14 @@ connection_db = psycopg2.connect(
 action_db = connection_db.cursor()
 
 while True:
-    print("1: Cadastrar Cliente")
-    print("2: Consultar Cliente")
+    print("1: Consultar Cliente")
+    print("2: Cadastrar Cliente")
     option = input("Digite uma opção: ")
 
     if option == '1':
-        register_client()
+        consult_customer.query_by_cpf(action_db)
     if option == '2':
-        consult_client()
+        register_customer.customer_personal_data(action_db, connection_db)
     if option == '0':
         break
 
